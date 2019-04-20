6c5f141e73080dbd7c62b058a5dab47cfff7371e
gojimmypi <gojimmypi@gmail.com>
2019-04-19 17:39:45 -0700

    initial brace matching

---
+++ BraceMatching/BraceMatchingTaggerProvider.cs
@@ -0,0 +1,29 @@
+﻿// adapted from https://github.com/madskristensen/ExtensibilityTools
+
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+
+namespace VerilogLanguage.BraceMatching
+{
+    [Export(typeof(IViewTaggerProvider))]
+    [ContentType("verilog")]
+    [TagType(typeof(TextMarkerTag))]
+    class BraceMatchingTaggerProvider : IViewTaggerProvider
+    {
+        // Step 2: Implement the CreateTagger method to instantiate a BraceMatchingTagger.
+        public ITagger<T> CreateTagger<T>(ITextView textView, ITextBuffer buffer) where T : ITag
+        {
+            if (textView == null)
+                return null;
+
+            //provide highlighting only on the top-level buffer
+            if (textView.TextBuffer != buffer)
+                return null;
+
+            return new BraceMatchingTagger(textView, buffer) as ITagger<T>;
+        }
+    }
+}

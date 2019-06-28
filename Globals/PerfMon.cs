4de0b7066a2b9f72fcb76cd0e99993cb49dcdc0c
gojimmypi <gojimmypi@gmail.com>
2019-06-28 16:24:18 -0700

    add CommandFilter_QueryStatus_Count

--- Globals/PerfMon.cs
+++ Globals/PerfMon.cs
@@ -13,6 +13,7 @@ namespace VerilogLanguage
         {
             public static int VerilogTokenTag_Count = 0;
             public static int VerilogTokenTagger_Count = 0;
+            public static int CommandFilter_QueryStatus_Count = 0;
 
             private static void init()
             {

474c8f14b1cfe0b7d8f17b39a9802fccd21c9416
gojimmypi <gojimmypi@gmail.com>
2019-06-28 12:00:44 -0700

    project file with new class files

---
+++ Globals/PerfMon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerilogLanguage
+{
+    public static partial class VerilogGlobals
+    {
+
+        public static class PerfMon
+        {
+            public static int VerilogTokenTag_Count = 0;
+            public static int VerilogTokenTagger_Count = 0;
+
+            private static void init()
+            {
+            }
+        }
+
+    }
+
+}

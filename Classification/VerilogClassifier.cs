e0798412affca15e0ea0b81beb4db15e23895638
gojimmypi <gojimmypi@gmail.com>
2019-06-30 10:36:49 -0700

    move namespace to VerilogLanguage.VerilogToken

--- Classification/VerilogClassifier.cs
+++ Classification/VerilogClassifier.cs
@@ -24,7 +24,7 @@
 //
 //***************************************************************************
 
-namespace VerilogLanguage
+namespace VerilogLanguage.VerilogToken
 {
     using System;
     using System.Collections.Generic;

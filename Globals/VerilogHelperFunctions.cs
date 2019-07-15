506e5220353cc15978da7677442e2099b1aa45fe
gojimmypi <gojimmypi@gmail.com>
2019-07-15 11:56:47 -0700

    more factor into smaller, related files

---
+++ Globals/VerilogHelperFunctions.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace VerilogLanguage
+{
+    public static partial class VerilogGlobals
+    {
+        // some characters, when encountered will need to have a full refresh, as they can have far-reaching consequences.
+        static public bool IsRefreshChar(string theString)
+        {
+            return (theString.Contains("/")) ||
+                   (theString.Contains("*")) ||
+                   (theString.Contains("[")) ||
+                   (theString.Contains("]")) ||
+                   (theString.Contains("}")) ||
+                   (theString.Contains("{")) ||
+                   (theString.Contains("(")) ||
+                   (theString.Contains(")"));
+        }
+
+        static public bool IsDelimeter(string theString)
+        {
+            return (theString == " ") ||
+                   (theString == "~") ||
+                   (theString == "[") ||
+                   (theString == "]") ||
+                   (theString == "}") ||
+                   (theString == "{") ||
+                   (theString == "(") ||
+                   (theString == ")") ||
+                   (theString == ";") ||
+                   (theString == ",") ||
+                   (theString == "@") ||
+                   (theString == "\"") ||
+                   (theString == "\t");
+        }
+
+        static public bool IsEndingDelimeter(string theString)
+        {
+            return (theString == "]") ||
+                   (theString == "}") ||
+                   (theString == ")");
+        }
+
+    }
+}
---
+++ Globals/VerilogParseState.cs
@@ -0,0 +1,95 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace VerilogLanguage
+{
+    public static partial class VerilogGlobals
+    {
+        /// <summary>
+        ///   VerilogParseState - while processing each segment, we'll keep track of attributes in a VerilogParseState 
+        /// </summary>
+        public struct VerilogParseState
+        {
+            public string thisItem;
+            public int thisIndex;
+            public string priorChar;
+            public string priorDelimiter;
+            public bool thisCharIsDelimiter;
+            public bool priorCharIsDelimiter;
+
+            public bool thisCharIsEndingDelimiter;
+            public bool priorCharIsIsEndingDelimiter;
+
+            public bool hasOpenSquareBracket;
+            public bool hasOpenRoundBracket;
+            public bool hasOpenSquigglyBracket;
+
+            public bool hasOpenDoubleQuote;
+
+            public bool IsNewDelimitedSegment;
+
+            private string _thisChar;
+            public string thisChar
+            {
+                get { return _thisChar; }
+                set
+                {
+                    _thisChar = value;
+                    thisCharIsDelimiter = IsDelimeter(value);
+                    thisCharIsEndingDelimiter = IsEndingDelimeter(value);
+                    priorCharIsDelimiter = IsDelimeter(priorChar);
+                    // note  contiguous spaces are a single segment
+                    IsNewDelimitedSegment = (thisCharIsDelimiter || priorCharIsDelimiter) && !((_thisChar == " ") && (priorChar == " "));
+
+                    if (IsNewDelimitedSegment)
+                    {
+
+                    }
+                    else
+                    {
+                        thisItem += thisChar;
+                    }
+                }
+            }
+
+            public void SetPriorValues()
+            {
+                // return;
+                priorCharIsDelimiter = thisCharIsDelimiter;
+                priorCharIsIsEndingDelimiter = thisCharIsEndingDelimiter;
+
+                priorChar = thisChar;
+                if (thisCharIsDelimiter)
+                {
+                    priorDelimiter = thisChar;
+                }
+            }
+
+
+            // initialize this VerilogParseState at creation time
+            public VerilogParseState(int i)
+            {
+                _thisChar = "";
+                thisIndex = 0;
+                thisItem = "";
+                IsNewDelimitedSegment = false;
+                priorChar = "";
+                priorDelimiter = "";
+                thisCharIsDelimiter = false;
+                priorCharIsDelimiter = false;
+                thisCharIsEndingDelimiter = false;
+                priorCharIsIsEndingDelimiter = false;
+                hasOpenSquareBracket = false;
+                hasOpenRoundBracket = false;
+                hasOpenSquigglyBracket = false;
+                hasOpenDoubleQuote = false;
+            }
+        }
+
+
+
+    }
+}

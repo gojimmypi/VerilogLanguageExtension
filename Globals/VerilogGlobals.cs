e4db66ecd6e1c87badcb2dafc9a0795f26cefede
gojimmypi <gojimmypi@gmail.com>
2019-07-19 12:06:14 -0700

    WIP working module declaration hover text

--- Globals/VerilogGlobals.cs
+++ Globals/VerilogGlobals.cs
@@ -46,6 +46,9 @@ namespace VerilogLanguage
         public static bool FoundDeclaration = false;
         public static string thisVariableDeclarationText = "";
         private static bool blnIsVariableExpected = false;
+        private static bool IsModuleDeclarationActive = false;
+        private static string thisModuleName = "";
+        private static string thisModuleDeclarationText = ""; 
 
         /// <summary>
         ///   InitHoverBuilder - prep for another refresh of hover item lookup
@@ -66,6 +69,10 @@ namespace VerilogLanguage
             FoundDeclaration = false;
             thisVariableDeclarationText = "";
             blnIsVariableExpected = false;
+
+            IsModuleDeclarationActive = false;
+            thisModuleDeclarationText = "";
+            thisModuleName = "";
         }
 
 
@@ -76,28 +83,35 @@ namespace VerilogLanguage
         /// <param name="s"></param>
         public static void BuildHoverItems(string s)
         {
-            void AddHoverItem()
+            void AddHoverItem(string ItemName, string HoverText)
             {
-                if (!VerilogVariables.Keys.Contains(thisHoverName) 
-                     && !IsDelimiter(thisHoverName) // never add a delimiter TODO - why would we even try? unresolved declaration naming?
-                     && thisHoverName != "" // never add a blank
-                   )
+                if (!VerilogVariables.Keys.Contains(ItemName) // we only have something to do if this variable does not exist in the lookup
+                     && !IsDelimiter(ItemName) // never add a delimiter TODO - why would we even try? unresolved declaration naming?
+                     && ItemName != "" // never add a blank
+                   ) // if
                 {
                     blnIsVariableExpected = false; // if we were expecting to find one, here it is!
 
-                    VerilogVariables.Add(thisHoverName, VerilogTokenTypes.Verilog_Variable);
+                    VerilogVariables.Add(ItemName, VerilogTokenTypes.Verilog_Variable);
 
-                    string thisHoverText = thisVariableHoverText + " " + thisHoverName;
-                    if (!VerilogGlobals.VerilogVariableHoverText.ContainsKey(thisHoverName))
+                    string thisHoverText = HoverText;
+                    if (IsModuleDeclarationActive)
                     {
-                        VerilogGlobals.VerilogVariableHoverText.Add(thisHoverName, thisHoverText);
+                        thisHoverText = "module " + thisModuleName + "(" + thisHoverText + ")";
+                        thisModuleDeclarationText += HoverText + System.Environment.NewLine;
+                    }
+
+                    if (!VerilogGlobals.VerilogVariableHoverText.ContainsKey(ItemName))
+                    {
+                        // add a new variable hover text attribute
+                        VerilogGlobals.VerilogVariableHoverText.Add(ItemName, thisHoverText);
                     }
                     else
                     {
                         // overwrite an existing variable declaration
-                        VerilogGlobals.VerilogVariableHoverText[thisHoverName] = thisHoverText;
-                    }
-                }
+                        VerilogGlobals.VerilogVariableHoverText[ItemName] = thisHoverText;
+                    }  
+                } // if
             }
 
             string thisTrimmedItem = (s == null) ? "" : s.Trim();
@@ -130,7 +144,7 @@ namespace VerilogLanguage
             //   input wire[1:1] k1, h1
             //
 
-            if (IsNaming)
+            if (IsNaming || IsModuleDeclarationActive) 
             {
                 //  when naming, all text, including blanks are appended to hover text
 
@@ -145,18 +159,45 @@ namespace VerilogLanguage
                 }
                 else
                 {
-                    if (FoundDeclaration)
+                    if (FoundDeclaration) 
                     {
-                        // we're naming a declaration, this is not a keyword, and we have a declaration
-                        // string such as "input wire [1:1], thus thisTrimmedItem must be a variable name.
-                        if (thisTrimmedItem == ",")
+                        // we're naming a declaration, this is not a keyword, and we have a declaration string
+                        // such as "input wire [1:1], thus thisTrimmedItem must be a variable name.
+                        if (IsModuleDeclarationActive && (thisModuleDeclarationText == "") && (thisTrimmedItem == "("))
                         {
-                            blnIsVariableExpected = true; // next variable expected (may be on next line?)
+                            // move the variable declaration into the module declaration
+                            // we may have intra-module declarations to follow
+                            thisModuleName = thisHoverName;
+
+                            thisModuleDeclarationText = thisVariableDeclarationText + " " + thisHoverName + " (" + System.Environment.NewLine;
+
+                            // clear the variable strings since we were naming a module
+                            thisHoverName = "";
+                            thisVariableDeclarationText = "";
+                            thisVariableHoverText = "";
+                            FoundDeclaration = false;
                         }
                         else
                         {
-                            thisHoverName = thisTrimmedItem;
-                            AddHoverItem();
+                            if (thisTrimmedItem == ",")
+                            {
+                                blnIsVariableExpected = true; // next variable expected (may be on next line?)
+                            }
+                            else if (thisTrimmedItem == ";")
+                            {
+                                // no more variables in a comma-delimited list with this declaration
+                                blnIsVariableExpected = false;
+                            }
+                            else if (thisTrimmedItem == ")")
+                            {
+                                // this is likely the end of a module definition (or a syntax error)
+                                thisModuleDeclarationText += thisTrimmedItem;
+                            }
+                            else
+                            {
+                                thisHoverName = thisTrimmedItem;
+                                AddHoverItem(thisHoverName, thisVariableHoverText + " " + thisHoverName);
+                            }
                         }
                     }
                     else
@@ -177,23 +218,12 @@ namespace VerilogLanguage
                             FoundDeclaration = true;
                             thisHoverName = thisTrimmedItem;
                         }
-                    }
-
-                    // what is this nonsense? See above. IsNaming is already true here:
-                    if (IsNaming && !FoundDeclaration)
-                    {
-                        if (FoundDeclaration)
-                        {
-                            // newly set FoundDeclaration, so we need to assemble the hover text with declaration and this variable
-                            thisVariableHoverText = thisVariableDeclarationText + " " + thisHoverName;
-                        }
-                        else
+                        if (!FoundDeclaration)
                         {
                             thisVariableHoverText += thisTrimmedItem;
                         }
+
                     }
-
-
                 }
 
                 if (IsNextNonblankName)
@@ -208,52 +238,46 @@ namespace VerilogLanguage
                 // when we are naming a veriable and end counter a semicolon or comma, we're done. add it and reset.
                 if ( (thisTrimmedItem == ";") || (thisTrimmedItem == ",") || (thisTrimmedItem == ")") || (thisTrimmedItem == "="))
                 {
-
-                    IsNaming = (thisTrimmedItem == "="); // set to false when we are done. all naming ends upon semi-colon.
-                                                         // an equals-sign needs the next value included. (e.g. reg val = 1'b0; )
-
-                    if (IsLastName && !FoundHoverName)
+                    if (IsModuleDeclarationActive && (thisTrimmedItem == ")"))
                     {
-                        // the name is the last, non-blank value before the semicolon. (e.g. "J2_AD_PORT" from "input [7:0] J2_AS_PORT;")
-                        IsLastName = (thisTrimmedItem != ","); // commas allow for multiple variables
-                        thisHoverName = lastKeyword;
-                        FoundHoverName = true;
+                        IsModuleDeclarationActive = false;
+                        AddHoverItem(thisModuleName, thisModuleDeclarationText);
                     }
-                    
-                    if (!IsNaming || (lastKeyword == ","))
-                    {
-                        AddHoverItem();
+                    else {
+                        // variable declaration
+                        IsNaming = (thisTrimmedItem == "="); // set to false when we are done. all naming ends upon semi-colon.
+                                                             // an equals-sign needs the next value included. (e.g. reg val = 1'b0; )
 
-                        //if (!VerilogVariables.Keys.Contains(thisHoverName) && thisHoverName != "")
-                        //{
-                        //    VerilogVariables.Add(thisHoverName, VerilogTokenTypes.Verilog_Variable);
-                        //    if (!VerilogGlobals.VerilogVariableHoverText.ContainsKey(thisHoverName))
-                        //    {
-                        //        VerilogGlobals.VerilogVariableHoverText.Add(thisHoverName, thisVariableHoverText);
-                        //    }
-                        //    else
-                        //    {
-                        //        VerilogGlobals.VerilogVariableHoverText[thisHoverName] = thisVariableHoverText;
-                        //    }
-                        //}
-                        // thisHoverName is the variable keyword and VerilogVariableHoverText is the definition text
-                        if (thisTrimmedItem == ",")
+                        if (IsLastName && !FoundHoverName)
                         {
-                            IsNaming = true;
-                            FoundHoverName = false;
+                            // the name is the last, non-blank value before the semicolon. (e.g. "J2_AD_PORT" from "input [7:0] J2_AS_PORT;")
+                            IsLastName = (thisTrimmedItem != ","); // commas allow for multiple variables
+                            thisHoverName = lastKeyword;
+                            FoundHoverName = true;
+                        }
+
+                        if (!IsNaming || (lastKeyword == ","))
+                        {
+                            AddHoverItem(thisHoverName, thisVariableHoverText + " " + thisHoverName);
+
+                            if (thisTrimmedItem == ",")
+                            {
+                                IsNaming = true;
+                                FoundHoverName = false;
+                            }
+                            else
+                            {
+                                thisVariableHoverText = "";
+                            }
+                            thisHoverName = "";
+                            IsNextNonblankName = false;
+                            thisTrimmedItem = ""; // once detected, we won't use it here
                         }
                         else
                         {
-                            thisVariableHoverText = "";
+                            AddHoverItem(thisHoverName, thisVariableHoverText + " " + thisHoverName);
                         }
-                        thisHoverName = "";
-                        IsNextNonblankName = false;
-                        thisTrimmedItem = ""; // once detected, we won't use it here
-                    }
-                    else
-                    {
-                        AddHoverItem();
-                    }
+                    } // else not IsModuleDeclarationActive
 
                 } // end if (thisTrimmedItem == ";")
 
@@ -268,6 +292,7 @@ namespace VerilogLanguage
                         IsNaming = true;
                         IsNextNonblankName = true;
                         thisVariableHoverText = thisTrimmedItem;
+                        IsModuleDeclarationActive = true;
                         break;
 
                     case "input":

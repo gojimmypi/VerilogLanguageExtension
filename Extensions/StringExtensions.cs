68e7d0e25494998d415a745a973b49c1ca4e4ef9
gojimmypi <gojimmypi@gmail.com>
2019-12-31 12:54:26 +0100

    added .left() and .right() string extensions

---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerilogLanguage 
+{
+    public static class StringExtensions
+    {
+        // thank you https://stackoverflow.com/questions/7574606/left-function-in-c-sharp/7574645
+        public static string Left(this string value, int maxLength)
+        {
+            if (string.IsNullOrEmpty(value)) return value;
+            maxLength = Math.Abs(maxLength);
+
+            return (value.Length <= maxLength
+                   ? value
+                   : value.Substring(0, maxLength)
+                   );
+        }
+
+        /// <summary>
+        /// Get substring of specified number of characters on the right.
+        /// </summary>
+        public static string Right(this string value, int length)
+        {
+            if (String.IsNullOrEmpty(value)) return string.Empty;
+
+            return value.Length <= length ? value : value.Substring(value.Length - length);
+        }
+    }
+}

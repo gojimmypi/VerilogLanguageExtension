4fa23ab78d81902f68019a6feaabb621980bb103
Barry Nolte <barry@barrynolte.com>
2020-05-05 14:45:12 -0700

    Added Project Template for Verilog
    Here's a project template for Verilog. It will create a new project from New/Project... and setup 'build/rebuild/clean' from the menu.  Deploy could become part of the build step, but is made difficult by VS because it wants to deploy through the remote debugger, and since that doesn't run on the FPGA, deploy is a foreign concept. I'm sure if you're going to add menu items, then running an arbitrary task from MSBuild (add to verilog.csproj) can't be that hard.  To use, build, then drop the resultant .zip file into your Templates\ProjectTemplates directory and you should have a Verilog project type.  Let me know if there's anything else you need.

---
+++ VerilogProjectTemplate/Verilog/Verilog1.v
@@ -0,0 +1,19 @@
+﻿//
+// Project:	     $safeprojectname$
+// Organization: $registeredorganization$
+// Date:         $time$
+//
+
+module gate(
+    input x,
+    input y,
+    output z
+    );
+	wire x_, y_, p,q;
+	not(x_, x);
+	not(y_, y);
+	and(p, x,y);
+	and(q, x_,y_);
+	or(z,p,q);
+ 
+endmodule

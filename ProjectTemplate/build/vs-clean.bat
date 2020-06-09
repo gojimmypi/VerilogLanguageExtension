eb1e8dc880eb717527f8f5e02b21b0fd5bf169a6
gojimmypi <gojimmypi@gmail.com>
2020-06-08 20:29:19 -0700

    WIP testing process output

--- ProjectTemplate/build/vs-clean.bat
+++ ProjectTemplate/build/vs-clean.bat
@@ -1,4 +1,22 @@
 echo off
+:: vs-clean.bat  [target] [Makefile]
+:: 
+:: example: vs-clean.bat ULX3S Makefile-ULX3S-12F.mk
+
+echo Param1=%1
+echo Param2=%2
+
+if "%1" == "" ( 
+    SET VBUILDTARGET=ULX3S
+    ) else (
+    SET VBUILDTARGET=%1
+)
+
+if "%2" == "" ( 
+    SET VBUILDMAKE=Makefile
+    ) else (
+    SET VBUILDMAKE=%2
+)
 
 IF EXIST "%windir%\Sysnative\wsl.exe" (
   SET VBUILDCMD="%windir%\Sysnative\wsl.exe"
@@ -12,5 +30,5 @@ IF EXIST "%windir%\Sysnative\wsl.exe" (
     )
 
 echo Calling %VBUILDCMD% make clean ...
-%VBUILDCMD% make clean
+%VBUILDCMD% make clean -f %VBUILDMAKE% %VBUILDTARGET%
 echo Done!
--- ProjectTemplate/build/vs-prog.bat
+++ ProjectTemplate/build/vs-prog.bat
@@ -1,5 +1,20 @@
 echo off
 
+if "%1" == "" ( 
+    SET VBUILDMAKE=Makefile
+    ) else (
+    SET VBUILDMAKE=%1
+)
+
+if "%2" == "" ( 
+    SET VBUILDTARGET=ULX3S
+    ) else (
+    SET VBUILDMAKE=%2
+)
+
+:: WSL is 64 bit and will only install on 64bit OS
+:: EXCEPT Visual Studio is 32 bit. So we neeeed to detect if this is being called
+:: from a 64bit command prompt, or a 32 bit Visual Studio process:
 IF EXIST "%windir%\Sysnative\wsl.exe" (
   SET VBUILDCMD="%windir%\Sysnative\wsl.exe"
   ) ELSE ( 
@@ -10,7 +25,15 @@ IF EXIST "%windir%\Sysnative\wsl.exe" (
 		exit 1
         )
     )
+:: https://docs.microsoft.com/en-us/visualstudio/msbuild/exec-task?view=vs-2019
+:: https://docs.microsoft.com/en-us/visualstudio/msbuild/msbuild-well-known-item-metadata?view=vs-2019
+:: https://docs.microsoft.com/en-us/visualstudio/msbuild/msbuild-project-file-schema-reference?view=vs-2019
+echo "Calling %VBUILDCMD% (make prog -f %VBUILDMAKE% %VBUILDTARGET%) & ..."
+echo "%SystemRoot%"
+:: "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe"
+start "Build Ping 1" /b ping -n 5 192.168.1.1
+:: start "Build" /b %VBUILDCMD% (make prog -f %VBUILDMAKE% %VBUILDTARGET%) &
+start "Build Ping 2" /b ping -n 5  192.168.1.2
+echo "Version %3"
 
-echo Calling %VBUILDCMD% make prog ...
-%VBUILDCMD% make prog
 echo Done!

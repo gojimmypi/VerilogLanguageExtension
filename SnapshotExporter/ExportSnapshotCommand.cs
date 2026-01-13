27302f973c50cdaec07bbf4544bec3fa85244b5d
gojimmypi <jim@wolfssl.com>
2026-01-12 17:15:05 -0800

    Initial working Snapshot Exporter Template

--- SnapshotExporter/ExportSnapshotCommand.cs
+++ SnapshotExporter/ExportSnapshotCommand.cs
@@ -25,217 +25,107 @@
 //
 //***************************************************************************
 
-using Microsoft.VisualStudio;
-using Microsoft.VisualStudio.ComponentModelHost;
-using Microsoft.VisualStudio.Editor;
+
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
-using Microsoft.VisualStudio.Text.Classification;
-using Microsoft.VisualStudio.Text.Editor;
-using Microsoft.VisualStudio.Text.Tagging;
-using Microsoft.VisualStudio.TextManager.Interop;
 using System;
 using System.ComponentModel.Design;
-using System.IO;
+using System.Globalization;
+using System.Threading;
 using System.Threading.Tasks;
+using Task = System.Threading.Tasks.Task;
 
-namespace VerilogLanguage.Testing
+namespace VSIXProject1
 {
-    internal sealed class ExportSnapshotCommand
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class SnapshotExporter
     {
-        private readonly AsyncPackage _package;
-        private readonly OleMenuCommand _command;
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
 
-        private ExportSnapshotCommand(AsyncPackage package, OleMenuCommandService commandService) {
-            _package = package;
+        /// <summary>
+        /// Command menu group (command set GUID). See the mathcing value in VerilogLanguagePackage.vsct:
+        ///   GuidSymbol name="guidVerilogLanguagePackageCmdSet"
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("C7F8B2F5-7A01-4A07-8E4B-4A29F77A0B9F");
 
-            var cmdId = new CommandID(GuidList.GuidVerilogLanguageCmdSet, PkgCmdIDList.CmdIdExportVerilogSnapshot);
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
 
-            _command = new OleMenuCommand(Execute, cmdId);
-            _command.BeforeQueryStatus += OnBeforeQueryStatus;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SnapshotExporter"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private SnapshotExporter(AsyncPackage package, OleMenuCommandService commandService) {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
 
-            commandService.AddCommand(_command);
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
         }
 
-        public static async Task InitializeAsync(AsyncPackage package) {
-            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static SnapshotExporter Instance
+        {
+            get;
+            private set;
+        }
 
-            object commandServiceObj = await package.GetServiceAsync(typeof(IMenuCommandService)).ConfigureAwait(true);
-            var commandService = commandServiceObj as OleMenuCommandService;
-            if (commandService == null) {
-                return;
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
             }
-
-            _ = new ExportSnapshotCommand(package, commandService);
         }
 
-        private void OnBeforeQueryStatus(object sender, EventArgs e) {
-            ThreadHelper.ThrowIfNotOnUIThread();
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package) {
+            // Switch to the main thread - the call to AddCommand in SnapshotExporter's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
 
-            // Always show/enable. Even if view acquisition fails, we show a message box.
-            _command.Visible = true;
-            _command.Enabled = true;
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new SnapshotExporter(package, commandService);
         }
 
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
         private void Execute(object sender, EventArgs e) {
             ThreadHelper.ThrowIfNotOnUIThread();
+            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+            string title = "Snapshot Exporter";
 
-            ThreadHelper.JoinableTaskFactory.Run(async () =>
-            {
-                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-
-                try {
-                    await ExecuteAsync().ConfigureAwait(true);
-                }
-                catch (Exception ex) {
-                    VsShellUtilities.ShowMessageBox(
-                        _package,
-                        ex.ToString(),
-                        "Export Verilog Snapshot - Exception",
-                        OLEMSGICON.OLEMSGICON_CRITICAL,
-                        OLEMSGBUTTON.OLEMSGBUTTON_OK,
-                        OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
-                }
-            });
-        }
-
-        private async Task ExecuteAsync() {
-            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-
-            IWpfTextView view = await TryGetActiveWpfTextView_NoAdaptersAsync().ConfigureAwait(true);
-            if (view == null) {
-                VsShellUtilities.ShowMessageBox(
-                    _package,
-                    "No active editor view found.",
-                    "Export Verilog Snapshot",
-                    OLEMSGICON.OLEMSGICON_WARNING,
-                    OLEMSGBUTTON.OLEMSGBUTTON_OK,
-                    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
-                return;
-            }
-
-            string filePath = await TryGetActiveDocumentPathAsync().ConfigureAwait(true);
-
-            object componentModelObj = await _package.GetServiceAsync(typeof(SComponentModel)).ConfigureAwait(true);
-            var componentModel = componentModelObj as IComponentModel;
-            if (componentModel == null) {
-                VsShellUtilities.ShowMessageBox(
-                    _package,
-                    "SComponentModel not available.",
-                    "Export Verilog Snapshot",
-                    OLEMSGICON.OLEMSGICON_WARNING,
-                    OLEMSGBUTTON.OLEMSGBUTTON_OK,
-                    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
-                return;
-            }
-
-            var classifierAgg = componentModel.GetService<IClassifierAggregatorService>();
-            var bufferAggFactory = componentModel.GetService<IBufferTagAggregatorFactoryService>();
-
-            if (classifierAgg == null || bufferAggFactory == null) {
-                VsShellUtilities.ShowMessageBox(
-                    _package,
-                    "Required editor services not available.",
-                    "Export Verilog Snapshot",
-                    OLEMSGICON.OLEMSGICON_WARNING,
-                    OLEMSGBUTTON.OLEMSGBUTTON_OK,
-                    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
-                return;
-            }
-
-            var exporter = new SnapshotExporter(classifierAgg, bufferAggFactory);
-
-            EditorSnapshotExport export = exporter.Export(view, filePath);
-
-            string outDir = Path.Combine(Path.GetTempPath(), "VerilogLanguageSnapshot");
-            string outFile = Path.Combine(outDir, MakeSafeFileName(filePath) + ".snapshot.json");
-            SnapshotExporter.WriteJson(export, outFile);
-
+            // Show a message box to prove we were here
             VsShellUtilities.ShowMessageBox(
-                _package,
-                "Wrote snapshot:\r\n" + outFile,
-                "Export Verilog Snapshot",
+                this.package,
+                message,
+                title,
                 OLEMSGICON.OLEMSGICON_INFO,
                 OLEMSGBUTTON.OLEMSGBUTTON_OK,
                 OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
-        }
-
-        private static string MakeSafeFileName(string filePath) {
-            if (string.IsNullOrEmpty(filePath)) {
-                return "untitled";
-            }
-
-            string name = Path.GetFileName(filePath);
-            foreach (char c in Path.GetInvalidFileNameChars()) {
-                name = name.Replace(c, '_');
-            }
-
-            return name;
-        }
-
-        // Avoid IVsEditorAdaptersFactoryService.
-        // Pull IWpfTextViewHost from IVsTextView via IVsUserData.
-        private async Task<IWpfTextView> TryGetActiveWpfTextView_NoAdaptersAsync() {
-            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-
-            object textManagerObj = await _package.GetServiceAsync(typeof(SVsTextManager)).ConfigureAwait(true);
-            var textManager = textManagerObj as IVsTextManager;
-            if (textManager == null) {
-                return null;
-            }
-
-            IVsTextView activeView;
-            int hr = textManager.GetActiveView(1, null, out activeView);
-            if (ErrorHandler.Failed(hr) || activeView == null) {
-                return null;
-            }
-
-            var userData = activeView as IVsUserData;
-            if (userData == null) {
-                return null;
-            }
-
-            object holder;
-            Guid guidViewHost = Microsoft.VisualStudio.Editor.DefGuidList.guidIWpfTextViewHost;
-
-            hr = userData.GetData(ref guidViewHost, out holder);
-            if (ErrorHandler.Failed(hr) || holder == null) {
-                return null;
-            }
-
-            var host = holder as IWpfTextViewHost;
-            if (host == null) {
-                return null;
-            }
-
-            return host.TextView;
-        }
-
-        private async Task<string> TryGetActiveDocumentPathAsync() {
-            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-
-            object monitorSelectionObj = await _package.GetServiceAsync(typeof(SVsShellMonitorSelection)).ConfigureAwait(true);
-            var monitorSelection = monitorSelectionObj as IVsMonitorSelection;
-            if (monitorSelection == null) {
-                return null;
-            }
-
-            int hr = monitorSelection.GetCurrentElementValue((uint)VSConstants.VSSELELEMID.SEID_DocumentFrame, out object frameObj);
-            if (ErrorHandler.Failed(hr) || frameObj == null) {
-                return null;
-            }
-
-            var frame = frameObj as IVsWindowFrame;
-            if (frame == null) {
-                return null;
-            }
-
-            hr = frame.GetProperty((int)__VSFPROPID.VSFPROPID_pszMkDocument, out object mkDoc);
-            if (ErrorHandler.Failed(hr)) {
-                return null;
-            }
-
-            return mkDoc as string;
         }
     }
 }
---
+++ VerilogLanguagePackage.cs
@@ -0,0 +1,82 @@
+﻿//***************************************************************************
+//
+//  MIT License
+//
+//  Copyright(c) 2025 gojimmypi
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+//***************************************************************************
+
+
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace VSIXProject1
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the
+    /// IVsPackage interface and uses the registration attributes defined in the framework to
+    /// register itself and its components with the shell. These attributes tell the pkgdef creation
+    /// utility what data to put into .pkgdef file.
+    /// </para>
+    /// <para>
+    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+    /// </para>
+    /// </remarks>
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid(VSIXProject1Package.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    public sealed class VSIXProject1Package : AsyncPackage
+    {
+        /// <summary>
+        /// VSIXProject1Package GUID string.
+        /// </summary>
+        public const string PackageGuidString = "8F4C0E2A-4A79-4C64-9B2E-6B2F6B0E1C19";
+
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
+        /// <param name="progress">A provider for progress updates.</param>
+        /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress) {
+            // When initialized asynchronously, the current thread may be a background thread at this point.
+            // Do any initialization that requires the UI thread after switching to the UI thread.
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await SnapshotExporter.InitializeAsync(this);
+        }
+
+        #endregion
+    }
+}
